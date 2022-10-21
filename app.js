8d0d030fc873058f31111827e5ebce79c214fb05
HDCOLOMBIA <73078081+HDCOLOMBIA@users.noreply.github.com>
2022-10-20 22:43:50 -0500

    let

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let a = 10;
-    b = 20
-    c = 'hola de nuevo'
-    x = 'Spiderman'
-    f = a + b
+let a = 10,
+    b = 20,
+    c = 'hola de nuevo',
+    x = 'Spiderman',
+    f = a + b;
 
 // console.log(a);
 // console.log(a);
@@ -14,10 +14,14 @@ console.log("x:", x)
 // objeto
 console.log( {x, a, b} )
 
-
 //avisos
 // console.log('imprimir en consola')
 // console.info('imprimir como tipo Informacion')
 // console.debug('imprimir como tipo Depuracion')
 // console.warn('imprimir como tipo Alerta')
 // console.error('imprimir como tipo Error')
+
+// contador
+for(i=0; i<10; i++){
+    console.count()
+}
