3419e265fcea9ed4d312a257e2bde852b8b5c3bc
HDCOLOMBIA <73078081+HDCOLOMBIA@users.noreply.github.com>
2022-10-20 22:31:12 -0500

    console log types

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ let a = 10;
 // console.log(a);
 // console.log(a);
 // console.log(a);
+
+// cadena
+console.log("x:", x)
+
+// objeto
+console.log( {x, a, b} )
+
+
+//avisos
+// console.log('imprimir en consola')
+// console.info('imprimir como tipo Informacion')
+// console.debug('imprimir como tipo Depuracion')
+// console.warn('imprimir como tipo Alerta')
+// console.error('imprimir como tipo Error')
