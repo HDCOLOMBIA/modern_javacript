9872267195b7e8fa3ea6514969620d339f893a6d
HDCOLOMBIA <73078081+HDCOLOMBIA@users.noreply.github.com>
2022-11-04 08:07:03 -0500

    update2

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,35 +1,16 @@
-//pop up
-alert('Hola desde App.js')
+//pop up - bloqueante
+// alert('Hola desde App.js');
 
+// prompt('Cual es tu nombre: ', 'Sin nombre');
 
-console.log(console.log('Hola. Mundo'));
+// let nombre = prompt('¿Cual es tu nombre: ', 'Sin nombre');
 
-let a = 10,
-    b = 20,
-    c = 'hola de nuevo',
-    x = 'Spiderman',
-    f = a + b;
+// let nombre = prompt('¿Cual es tu nombre: '); // null when user cancel
+// console.log(nombre);
+// console.log('****' + nombre + '****');
 
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(x);
-console.log(f);
+//Bloqueante
+// const seleccion = confirm('¿esta seguro de borrar esto?');
+// console.log(seleccion);
 
-// cadena
-console.log("x:", x)
-
-// objeto
-console.log( {x, a, b} )
-
-//avisos
-console.log('imprimir en consola')
-console.info('imprimir como tipo Informacion')
-console.debug('imprimir como tipo Depuracion')
-console.warn('imprimir como tipo Alerta')
-console.error('imprimir como tipo Error')
-
-// contador
-for(i=0; i<10; i++){
-    console.count()
-}
+// console.log(global);
