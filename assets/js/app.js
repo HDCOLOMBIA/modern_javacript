4452601a72c85ffbbe9a6aa2f3ac4a6e85011e4d
HDCOLOMBIA <73078081+HDCOLOMBIA@users.noreply.github.com>
2022-11-03 21:49:40 -0500

    modern

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,48 @@
-//pop up
-alert('Hola desde App.js')
+// console.log(miNuevoNombre + ' Lara');
+
+// console.log(console.log('Hola. Mundo'));
+
+// let a = 10,
+//     b = 20,
+//     c = 'hola de nuevo',
+//     x = 'Spiderman',
+//     f = a + b;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(x);
+// console.log(f);
+
+// // cadena
+// console.log("x:", x)
+
+// // objeto
+// console.log( {x, a, b} )
+
+// //avisos
+// console.log('imprimir en consola')
+// console.info('imprimir como tipo Informacion')
+// console.debug('imprimir como tipo Depuracion')
+// console.warn('imprimir como tipo Alerta')
+// console.error('imprimir como tipo Error')
+let anime = [["Akatsuki", "Pain"], ["Black", "Asta"], ["DBZ", "Cell"]]
+console.table(anime);
+
+// // contador
+// for(i=0; i<10; i++){
+//     console.count("Jeiller")
+// }
+
+// var outerHeight = 1000000;
+// var outerWidth = 100;
+
+// let  outerHeight = 152256;
+// const  outerWidth = 616;
+// let miNuevoNombre = 'Jeiller';
+
+// console.log('miNuevoNombre', miNuevoNombre);
 
 
-console.log(console.log('Hola. Mundo'));
-
-let a = 10,
-    b = 20,
-    c = 'hola de nuevo',
-    x = 'Spiderman',
-    f = a + b;
-
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(x);
-console.log(f);
-
-// cadena
-console.log("x:", x)
-
-// objeto
-console.log( {x, a, b} )
-
-//avisos
-console.log('imprimir en consola')
-console.info('imprimir como tipo Informacion')
-console.debug('imprimir como tipo Depuracion')
-console.warn('imprimir como tipo Alerta')
-console.error('imprimir como tipo Error')
-
-// contador
-for(i=0; i<10; i++){
-    console.count()
-}
+// desestructuración de variables
+// console.log({outerHeight,outerWidth,miNuevoNombre});
