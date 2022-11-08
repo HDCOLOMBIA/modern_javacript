8c08e9018814cb250c4dac231bf15b9b81424dcf
HDCOLOMBIA <jdcastro08@misena.edu.co>
2022-11-07 21:10:57 -0500

    update primitives types

--- Fundamentos/primitivos.js
+++ Fundamentos/primitivos.js
@@ -1,2 +1,49 @@
-let nombre = 'Peter Parker';
-console.log(nombre);
+// let nombre = 'Peter Parker';
+// console.log(nombre);
+
+// let nombre = 'Ben Parker'
+// console.log(nombre);
+
+// var nombre = 'Peter Parker';
+// console.log(nombre);
+
+// var nombre = 'Ben Parker'
+// console.log(nombre);
+
+// String texto
+// Tipo
+// nombre = "Tía Maya";
+// nombre = 'Tía Maya';
+
+// console.log(typeof nombre);
+
+// nombre  = 132;
+// console.log(typeof nombre);
+
+// Boleanos
+// let esMarvel = false;
+// console.log(typeof esMarvel);
+
+// Números
+// let edad = 33;
+// console.log(typeof edad);
+
+// edad = 33.001;
+// log(typeof edad);
+
+// Camel Case  ejemplo; elPoderDeSpiderman
+
+// undefined , sin valor
+// let superPoder;
+// console.log(typeof superPoder);
+
+// Soy null
+// let soyNull = null;
+// console.log(typeof soyNull);
+
+// Symbols
+// let symbol1 = Symbol('a');
+// let symbol2 = Symbol('a');
+// console.log(typeof symbol1);
+
+// console.log(symbol1 === symbol2);
