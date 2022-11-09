d7c6e600d865f0eb5ad57f99a2997abdf8b03a48
HDCOLOMBIA <73078081+HDCOLOMBIA@users.noreply.github.com>
2022-11-09 07:52:49 -0500

    update reserverd

--- Fundamentos/palabras-reservadas.js
+++ Fundamentos/palabras-reservadas.js
@@ -1,1 +1,50 @@
 console.log('Hola mundo');
+
+// let constante = 123;
+// console.log(constante);
+
+// let await = 123;
+// console.log(await);
+
+// let π = Math.PI;
+// console.log(π);
+
+// var 〱〱 = 2;
+// console.log(〱〱);
+
+// caracteres raros
+// var Hͫ̆̒̐ͣ̊̄ͯ͗͏̵̗̻̰̠̬͝ͅE̴̷̬͎̱̘͇͍̾ͦ͊͒͊̓̓̐_̫̠̱̩̭̤͈̑̎̋ͮͩ̒͑̾͋͘Ç̳͕̯̭̱̲̣̠̜͋̍O̴̦̗̯̹̼ͭ̐ͨ̊̈͘͠M̶̝̠̭̭̤̻͓͑̓̊ͣͤ̎͟͠E̢̞̮̹͍̞̳̣ͣͪ͐̈T̡̯̳̭̜̠͕͌̈́̽̿ͤ̿̅̑Ḧ̱̱̺̰̳̹̘̰́̏ͪ̂̽͂̀͠ = 'Zalgo';
+
+// Las palabras reservadas toman color
+
+// if
+// try
+
+// while
+// for()
+
+//enum, no cambia de color
+
+// Underscore
+// let _ = 123;
+
+// dolar
+// let $ = 123;
+// let objeto$ = 123;
+// let objeto$21 = 123;
+
+// No lleva puntos
+// let objeto.123 = 123;
+// let precio_99_99 = 123;
+
+// Nombres significativo
+// let x = 'Jeiller';
+// let jugadorConPuntajeMasAlto = 'Jeiller';
+
+// open camel case
+// class JuegoAnio {
+
+// }
+
+
+
