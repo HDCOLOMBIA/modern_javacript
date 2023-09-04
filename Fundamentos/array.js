14bea4136c55b6a97c12c640864adcb3a4c9b973
HDCOLOMBIA <73078081+HDCOLOMBIA@users.noreply.github.com>
2023-09-03 23:53:38 -0500

    arreglos1

--- Fundamentos/array.js
+++ Fundamentos/array.js
@@ -4,8 +4,10 @@
 // const arr = [];
 // console.log(arr);
 
-let videojuegos = ['Super Mario 94','Megaman','God of war 1-4', 'Halo 1-6'];
-console.log({videojuegos});
+let videojuegos = ['Super Mario 94', 'Megaman', 'God of war 1-4', 'Halo 1-6'];
+console.log({ videojuegos });
+console.log(videojuegos[0]);
+
 
 
 let arregloCosas = [
@@ -13,16 +15,17 @@ let arregloCosas = [
     123,
     'Jeiller',
     1 + 2,
-    function(){},
-    ()=>{},
-    {a:1},
+    function(){}, //Función tradicional
+    ()=>{}, //Función de flecha
+    {a:1}, //Objeto literal
     ['Kratos','Megaman','Doom','Master chief',
     ['Lara Croft', 'Sonic']]
 ];
 
-// console.log(arregloCosas);
-// console.log(arregloCosas[0]);
-// console.log(arregloCosas[2]);
+console.log(arregloCosas);
+console.log(arregloCosas[0]);
+console.log(arregloCosas[2]);
 // ver el valor de un array dentro de un array
-// console.log(arregloCosas[7][2]);
-console.log(arregloCosas[7][4][1]);
+console.log(arregloCosas[7][3]); //
+// ver el valor de un array dentro de un array, que está en otro array
+console.log(arregloCosas[7][4][0]);
